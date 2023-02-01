9c60d0e6dc6af98da5171a1bc96785a7a3598c0f
Odette Gleen <nkargleeny@gmail.com>
2023-02-01 23:18:28 +0100

    carried out the request test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  let(:user) { User.create(id: 2, Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.') }
+  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico', posts_counter: 3) }
 
   it 'should get index' do
     get '/users'
@@ -9,12 +9,12 @@ RSpec.describe 'Users', type: :request do
   end
 
   it 'should get show' do
-    get "/users#{user.id}"
+    get "/users/#{user.id}"
     expect(response).to have_http_status(200)
   end
 
   it 'return correct show template' do
-    get "/users#{user.id}"
+    get "/users/#{user.id}"
     expect(response).to render_template(:show)
   end
 
@@ -24,12 +24,12 @@ RSpec.describe 'Users', type: :request do
   end
 
   it 'gets correct response body' do
-    get "/users#{user.id}"
-    expect(response.body).to include('Here are the details of a particular user')
+    get "/users/#{user.id}"
+    expect(response.body).to include('This is a user')
   end
 
   it 'gets correct response body' do
     get '/users'
-    expect(response.body).to include('Here is a list of all users')
+    expect(response.body).to include('Here is a list of all the users')
   end
 end
