cc7261c9689ff069b2e057644836f0f516c2b059
FrederickMih <fredtimermih@gmail.com>
2023-02-12 03:20:36 +0100

    Fix conflict issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,13 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
 
   after_save :update_comments_counter
 
+  private
+
   def update_comments_counter
-    Post.increment_counter(:comments_counter, post_id)
+    post.increment!(:comments_counter)
   end
 end
